b8e45134413f4b8b74851d8cff35bcd9b4641ac6
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-07-10 16:43:22 -0400

    password validator

--- src/Validator.js
+++ src/Validator.js
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+  handleEmail(e) {
+    this.setState({email: e.target.value})
+  }
+
+  handlePassword(e) {
+    this.setState({password: e.target.value})
+  }
+
+  handleConfirmPassword(e) {
+    this.setState({confirmPassword: e.target.value})
+  }
+
+  handleClick(e){
+    if (this.state.password !== this.state.confirmPassword) {
+      alert("Passwords do not match!")
+    }else {
+      alert("Valid Password!")
+    }
+  }
   render() {
     return (
       <div className="form">
         <h1>Sign Up</h1>
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="confirm password" />
-        <input type="submit" value="Submit" />
+        <input type="text" placeholder="email" onChange={(e) => this.handleEmail(e)}/>
+        <input type="password" placeholder="password" onChange={(e) => this.handlePassword(e)}/>
+        <input type="password" placeholder="confirm password" onChange={(e) => this.handleConfirmPassword(e)}/>
+        <input onClick={(e) => this.handleClick(e)} type="submit" value="Submit" />
       </div>
     );
   }
